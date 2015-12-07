664969090ba9a51af27d23624214c7bb2994bf6b
Hans Duedal <hd@onlinecity.dk>
2015-12-07 14:43:17 +0100

    Add PDF gen and upload

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'gatewayapi.com'
-copyright = u'2014, OnlineCity Dev Team'
+copyright = u'2015, OnlineCity'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -201,13 +201,13 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'httppost.tex', u'httppost Documentation',
-   u'OnlineCity Dev Team', 'manual'),
+  ('index', 'gatewayapi.tex', u'GatewayAPI.com Documentation',
+   u'OnlineCity', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'gatewayapi-stacked.pdf'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
@@ -231,8 +231,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'httppost', u'httppost Documentation',
-     [u'OnlineCity Dev Team'], 1)
+    ('index', 'gatewayapi', u'GatewayAPI.com Documentation',
+     [u'OnlineCity'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -245,8 +245,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'httppost', u'httppost Documentation',
-   u'OnlineCity Dev Team', 'httppost', 'One line description of project.',
+  ('index', 'gatewayapi', u'GatewayAPI.com Documentation',
+   u'OnlineCity', 'gatewayapi', 'GatewayAPI.com SMS and MMS Gateway.',
    'Miscellaneous'),
 ]
 
--- docs/index.rst
+++ docs/index.rst
@@ -3,8 +3,8 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to httppost's documentation!
-====================================
+GatewayAPI.com documentation
+============================
 
 Contents:
 
